bdd1afa12f2437ed279c727c6c300e48e53df128
jayaram <jayaramgopi143@gmail.com>
2026-04-15 18:49:45 +0000

    first devops training commit

---
+++ script1.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "Hello Jayaram - /DevOps Automation"
+date
+whoami
---
+++ script2.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+name="Jayaram"
+
+echo "Welcome $name to DevOps training"
+echo "Today is:"
+date
